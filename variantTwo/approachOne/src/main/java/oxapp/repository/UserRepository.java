cb60741f772eb87efa2635bad6815c4682aed094
Konrad <krukarkonrad@gmail.com>
2020-03-25 13:30:08 +0100

    clean

--- variantTwo/approachOne/src/main/java/oxapp/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package oxapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import oxapp.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
